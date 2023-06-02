d7543ed9b735fcf2e06bcc83f9617b95450a4e1a
Kiryl <66584728+sputnikpershi@users.noreply.github.com>
2023-06-02 21:50:47 +0800

    added function returning commission

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,61 @@
 @file:Suppress("DEPRECATION")
 
-import java.util.Date
-import kotlin.math.min
-
 fun  main () {
-    agoToText(secAgo = 259200)
+
+    /*
+    Задача №1. Только что
+    Социальные сети и мессенджеры показывают, когда ваш собеседник последний раз был онлайн:
+
+    Задача №2. Разная комиссия
+     */
+
+    //agoToText(secAgo = 259200)
+    println(getCommission(type = "maestro", historySum = 0, 299))
+}
+
+
+fun getCommission(type: String = "VK Pay", historySum: Int, sumWillSend: Int): Double {
+    var commission = 0.0
+    val totalSum = historySum + sumWillSend
+    val text = type.lowercase()
+    when (text) {
+        "master card" -> commission = commissionForCardMaster(amount = totalSum, sumForSend = sumWillSend, percent = 0.006, addition = 20)
+        "maestro" -> commission = commissionForCardMaster(amount = totalSum, sumForSend = sumWillSend, percent = 0.006, addition = 20)
+        "visa" -> commission = sendMoneyTax(amount = sumWillSend, percent = 0.0075, minTax = 35)
+        "mir" ->  commission = sendMoneyTax(amount = sumWillSend, percent = 0.0075, minTax = 35)
+        "vk pay" -> commission = 0.0
+        else -> println("Платеж не может быть обработан")
+    }
+    return commission
+}
+
+fun sendMoneyTax (amount: Int, percent: Double, minTax : Int): Double  {
+    val value = amount*percent
+    val result =  if ( value > 35) {
+        value
+    } else {
+        minTax.toDouble()
+    }
+    println("Tax equal to $result")
+    return result
+}
+
+fun commissionForCardMaster (amount: Int, sumForSend: Int, percent: Double, addition: Int): Double  {
+    val value = sumForSend*percent
+    val minTax = 20
+    return if (amount < 300 || amount > 75000) {
+        value + minTax.toDouble()
+    } else {
+        0.0
+    }
 }
 
 fun agoToText(secAgo: Int) = println(returnDateText(secAgo))
 
 fun returnDateText(seconds: Int): String {
     var result = ""
-    var min = seconds/60
-    var hour = seconds/60/60
+    val min = seconds/60
+    val hour = seconds/60/60
     var days = seconds/60/60/24
     val hourText = returnMinText(hour = hour)
     val minText = returnMinText(min = min)
