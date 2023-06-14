017a11659e4503ef8f82177d7effdc658863e648
Kiryl <66584728+sputnikpershi@users.noreply.github.com>
2023-06-14 08:12:34 +0800

    set addition tests

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -15,9 +15,63 @@ class MainTest {
     }
 
     @Test
+    fun checkTransferCardMasterCardWithZeroHistory() {
+        val monthSum = 0.0
+        val transferSum = 10001.0
+        val result = transferMoney(historySum = monthSum, sendSum = transferSum, type = "master card")
+        assertEquals(expected = 0.0, actual = result)
+    }
+
+    @Test
+    fun checkTransferCardMaestroWithZeroHistory() {
+        val monthSum = 0.0
+        val transferSum = 10001.0
+        val result = transferMoney(historySum = monthSum, sendSum = transferSum, type = "maestro")
+        assertEquals(expected = 0.0, actual = result)
+    }
+    @Test
+    fun checkTransferCardMaestroWithCommission() {
+        val monthSum = 70000.0
+        val transferSum = 10001.0
+        val result = transferMoney(historySum = monthSum, sendSum = transferSum, type = "maestro")
+        assertEquals(expected = 80.006, actual = result)
+    }
+    @Test
+    fun checkTransferCardVisaWithZeroHistory() {
+        val monthSum = 0.0
+        val transferSum = 100001.0
+        val result = transferMoney(historySum = monthSum, sendSum = transferSum, type = "visa")
+        assertEquals(expected = 750.0074999999999, actual = result)
+    }
+
+    @Test
+    fun checkTransferCardMirWithZeroHistory() {
+        val monthSum = 0.0
+        val transferSum = 100001.0
+        val result = transferMoney(historySum = monthSum, sendSum = transferSum, type = "mir")
+        assertEquals(expected = 750.0074999999999, actual = result)
+    }
+
+
+    @Test
     fun checkTransferVKLimit() {
         val monthSum = 30000.0
         val transferSum = 13000.0
+        val result = transferMoney(historySum = monthSum, sendSum = transferSum, type = "vk pay")
+        assertEquals(expected = ERROR_VK_LIMIT, actual = result)
+    }
+
+    @Test
+    fun checkTransferVKZeroCommission() {
+        val monthSum = 0.0
+        val transferSum = 10000.0
+        val result = transferMoney(historySum = monthSum, sendSum = transferSum, type = "vk pay")
+        assertEquals(expected = 0.0, actual = result)
+    }
+    @Test
+    fun checkTransferVKZeroCommissionLimit() {
+        val monthSum = 400.0
+        val transferSum = 100000.0
         val result = transferMoney(historySum = monthSum, sendSum = transferSum, type = "vk pay")
         assertEquals(expected = ERROR_VK_LIMIT, actual = result)
     }
